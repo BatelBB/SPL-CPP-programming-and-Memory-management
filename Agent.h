59823c11c77d3b434e07dc967a24058263bb5c73
Batel Shkolnik <batelx13@gmail.com>
2020-11-04 19:26:03 +0200

    Update Agent.h

--- Agent.h
+++ Agent.h
@@ -2,20 +2,20 @@
 #define AGENT_H_
 
 #include <vector>
-
+//I'm not sure about this class at all, it represents the different agents, not sure what it does.
 class Agent{
 public:
     Agent(Session& session);
-    
+    virtual Agent* clone()=0;
     virtual void act()=0;
-private:
+protected:
     Session& session;
 };
 
 class ContactTracer: public Agent{
 public:
     ContactTracer(Session& session);
-    
+    virtual Agent* clone();
     virtual void act();
 };
 
@@ -23,9 +23,9 @@ public:
 class Virus: public Agent{
 public:
     Virus(int nodeInd, Session& session);
-    
+    virtual Agent* clone();
     virtual void act();
-private:
+protected:
     const int nodeInd;
 };
 
