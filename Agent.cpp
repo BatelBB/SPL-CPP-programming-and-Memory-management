d40614010812705a156bb50065e5dbc02a29961e
Batel Shkolnik <batelx13@gmail.com>
2020-11-04 19:25:41 +0200

    Update Agent.cpp

--- Agent.cpp
+++ Agent.cpp
@@ -3,20 +3,26 @@
 
 
     Agent(Session& session);
-    
+     virtual Agent* clone(){
+        return new Agent(*this);
+    };
     virtual void act()=0;
 
 
 
     ContactTracer(Session& session);
-    
+    virtual Agent* clone(){
+        return new ContactTracer(*this);
+    };
     virtual void act();
 
 
 
 
     Virus(int nodeInd, Session& session);
-    
+    virtual Agent* clone(){
+        return new Virus(*this);
+    };
     virtual void act();
 
 
