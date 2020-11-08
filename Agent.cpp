dfa71fc89ad325646079d7df3000e50655b738e4
Batel Shkolnik <batelx13@gmail.com>
2020-11-08 22:09:49 +0200

    Update Agent.cpp

--- Agent.cpp
+++ Agent.cpp
@@ -1,28 +1,78 @@
 #include <iostream>
 #include "Agent.h"
+#include "Tree.h"
+#include "Session.h"
 
+Agent::Agent(Session &session) : session(session)
+{
+}
+Agent::Agent *clone()
+{
+    return new Agent(*this);
+}
+void Agent::act()
+{
+}
 
-    Agent(Session& session);
-     virtual Agent* clone(){
-        return new Agent(*this);
-    };
-    virtual void act()=0;
+ContactTracer::ContactTracer(Session &session) : Agent(session)
+{
+}
+Agent *ContactTracer::clone()
+{
+    return new ContactTracer(*this);
+}
 
+void ContactTracer::act()
+{
+    int currentNode = session.dequeueInfected();
 
+    if (currentNode != -1)
+    {
+        Tree *currTree = session.BFS(currentNode);
+        int nodeToRemove = currTree->traceTree();
+        session.remove(nodeToRemove);
+        delete currTree;
+    }
+}
+int ContactTracer::getNodeIndex() const
+{
+    return -1;
+}
 
-    ContactTracer(Session& session);
-    virtual Agent* clone(){
-        return new ContactTracer(*this);
-    };
-    virtual void act();
+Virus::Virus(int nodeInd, Session &session) : Agent(session), nodeInd(nodeInd)
+{
+}
 
+Agent *Virus::clone()
+{
+    return new Virus(*this);
+}
 
+int Virus::getNodeIndex() const
+{
+    return nodeInd;
+}
 
+//This method does 2 things:
+//first it checks if the node that it's currently in is infected - if not then it makes it infected - it takes from Session.h the "void enqueueInfected(int)"
+//the second thing is that it goes through the node's neighbor, goes in ascending order and choose the smaller one to settle - just put the virus in it.
+void Virus::act()
+{
+    if (!session.isInfected(nodeInd))
+    {
+        session.infectNode(nodeInd);
+        session.enqueueInfected(nodeInd);
+    }
 
-    Virus(int nodeInd, Session& session);
-    virtual Agent* clone(){
-        return new Virus(*this);
-    };
-    virtual void act();
+    std::vector<int> neighbors = session.getNodeNeighbors(nodeInd);
 
-
+    for (int i = 0; i < neighbors.size(); i++)
+    {
+        if (!session.isVirusCarrier(neighbors[i]))
+        {
+            Virus spreadVirus(neighbors[i], session);
+            session.addAgent(spreadVirus);
+            break;
+        }
+    }
+}
