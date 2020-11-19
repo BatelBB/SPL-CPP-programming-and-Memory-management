d89b1601c46c1b87501748363dd8025cc96afeee
Batel Shkolnik <batelx13@gmail.com>
2020-11-19 12:04:13 +0200

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,42 +1,34 @@
 #ifndef GRAPH_H_
-	#define GRAPH_H_
-	
+#define GRAPH_H_
 
-	#include <vector>
-	
 
-	class Graph{
-	public:
-	    // need default constructor for session constructor, as meni said in lecture 6 when someone asked
-	    Graph();
-	
+#include <vector>
+#include "json.hpp"
+#include "Tree.h"
+#include <queue>
 
-	    Graph(std::vector<std::vector<int>> matrix); 
-	    
-	    void infectNode(int nodeInd);
-	    bool isInfected(int nodeInd);
-	
+class Graph {
+public:
+    Graph(std::vector<std::vector<int>> matrix);
 
-	    //getNeighbors should return a sorted list of node ids that are adjacent to nodeInd
-	    std::vector<int> getNeighbours(int nodeInd) const;
-	
+    void infectNode(int nodeInd);
 
-	    // isolate should remove all links to adjacent nodes to the given node
-	    void isolate(int nodeInd);
-	
+    bool isInfected(int nodeInd);
 
-	    int size() const;
-	
+    std::vector<int> getNodeNeighbors(int nodeInd) const;
 
-	    std::vector<std::vector<int>> getEdges() const;
-	    std::vector<int> getInfected() const; 
-	
+    void separateNode(int nodeInd);
 
-	private:
-	    std::vector<std::vector<int>> edges;
-	    std::vector<bool> infected;
-	};
-	
+    nlohmann::json writejson();
 
-	#endif
+    Tree *BFS(int nodeInd, const Session &session) const;
+
+private:
+    std::vector<std::vector<int>> edges;
+
+
+    std::vector<bool> infected;
+};
+
+#endif
 
